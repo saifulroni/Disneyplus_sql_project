81a9108f3d4979e703c3bc16047b1d272e5e47c2
saifulroni <saifulislamroni119@gmail.com>
2025-11-07 03:24:55 +0600

    Add files via upload

---
+++ Schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE disneyplus (
+	show_id 	VARCHAR (10),
+	type 		VARCHAR (10),
+	title		VARCHAR (200),
+	director 	VARCHAR (200),
+	casts		VARCHAR (1000),
+	country		VARCHAR (150),
+	date_added 	VARCHAR (50),
+	release_year INT,
+	rating		VARCHAR (20),
+	duration	VARCHAR (20),
+	listed_in	VARCHAR (100),
+	description VARCHAR (250)
+
+);
